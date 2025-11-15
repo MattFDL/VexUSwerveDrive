ede5345ad25efa59db13c839acc553240cea6016
MATTHEW_JANKE <jankemat000@fondystudents.org>
2025-11-15 17:29:20 -0600

    Fixed Merge Issues (I hope)

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -12,24 +12,17 @@ public:
     double position_rotation_sensor = 0;
     double adjusted_rotation = 0;
 
-    // use IMU for secondary heading claulation
-    bool use_imu = true;
-
-
     double position_rotation_imu = 0; // imu heading
     double forward_count_pre = 0;     // forward rotation sensor previous count
     double rotation_count_pre = 0;    // sideways rotation sensor previous count
 
+    // use IMU for secondary heading claulation
+    bool use_imu = false;
+    double current_reading_imu_pid = 0; //Unused Currently, might be implemented later
 
-    // constants for chasis
-    const double WHEEL_DIAMETER = 2.132;                // diameter of tracking wheel in inches
-    //!this is never used
-    //? can we use your ticks to deterimine this value more acurately
-    //
-    const double ROTATION_DISTANCE_WHEEL_RADIUS = 6.25; // distance from center of rotation to the rear tracking wheel in inches
-
-    // const double fudge_factor_rotation = 1.2857;
-
+    //CONSTANTS FOR DISTANCE CALCULATIONS
+    #define ROTATION_SENSOR_TICKS_TO_FULL_TURNS 143580.0 //This value was determined experimentally (in 360 turns)
+    #define FORWARD_SENSOR_TICKS_TO_INCHES 5243.0 //This value was determined experimentally (in inches)
 
     double test = 0; // testing purposes curently unused
 
@@ -38,49 +31,38 @@ public:
     pros::Rotation sidewaysRotation; // rear rotation sensor
     pros::IMU imu;                   // inertial measurement unit
 
+    // Constructor
+    odometry(pros::Rotation &forward, pros::Rotation &sideways, pros::IMU &i) : forwardRotation(forward), sidewaysRotation(sideways), imu(i) {}
 
-    // PID VALUES
-    double current_reading_imu_pid = 0;
-
-
-    // constructor
-    odometry(pros::Rotation &forward, pros::Rotation &sideways, pros::IMU &i) : forwardRotation(forward), sidewaysRotation(sideways), imu(i)
-    {
-    }
-
-    void reset_sensors()
-    {
-        forwardRotation.reset(); //* DO NOT PUT IN CONSTRUCTOR PLEASE THIS CRASHES THINGS DONT REALLY KNOW WHY
+    void reset_sensors() {
+        forwardRotation.reset(); //* DO NOT PUT IN CONSTRUCTOR PLEASE THIS CRASHES THINGS
         forwardRotation.reset_position();
         sidewaysRotation.reset();
         sidewaysRotation.reset_position();
         imu.reset();
     }
 
-    void calculate_postition()
-    {
-        double rotation_count_turns = (static_cast<double>(sidewaysRotation.get_position()) / 143580.0); //This value was determined experimentally (in 360 turns)
-        double forward_count_turns = (static_cast<double>(forwardRotation.get_position()) / 5243.0); //This value was determined experimentally (in inches)
-        test = forward_count_turns;
-        
-
+    void calculate_postition() {
+        double rotation_count_turns = (static_cast<double>(sidewaysRotation.get_position()) / ROTATION_SENSOR_TICKS_TO_FULL_TURNS); 
+        double forward_count_inches = (static_cast<double>(forwardRotation.get_position()) / FORWARD_SENSOR_TICKS_TO_INCHES); 
+        test = forward_count_inches;
+    
         double deltaRotationSensorTurns = rotation_count_turns - rotation_count_pre;
         double deltaDegrees = deltaRotationSensorTurns * 360.0;
         
         position_rotation_sensor += deltaDegrees;
 
-        // change in foraward distance
-        double deltaForwardSensorTurns = forward_count_turns - forward_count_pre;
-        double deltaPos = deltaForwardSensorTurns; //* WHEEL_DIAMETER * M_PI);
+        // change in forward distance
+        double deltaPos = forward_count_inches - forward_count_pre; 
 
         // update previous counts
         rotation_count_pre = rotation_count_turns;
-        forward_count_pre = forward_count_turns;
+        forward_count_pre = forward_count_inches;
         /* #endregion */
 
         // update heading based on either imu or rotation sensor
         /* #region update heading */
-        // angels
+        // angles
         /*
         !   you convert turns to degrees here to then keep track of position_rotation_sensor in degrees
         !   then later you convert to radians without ever using position_rotation_sensor
@@ -88,11 +70,11 @@ public:
         ?   deltaRad is also not needed it is declared to just add
         !   the rotation sensor is in turns of the wheel not 
         */
-        double deltaRad = deltaRotationSensorTurns * 360.0;
-        position_rotation_sensor += deltaRad;
 
         // calculate current heading in radians
-        double position_rotation_rad = position_rotation_sensor * (M_PI / 180.0);
+        adjusted_rotation = inputModulus(position_rotation_sensor, -180, 180);
+
+        double position_rotation_rad = adjusted_rotation * (M_PI / 180.0);
         if (use_imu)
         {
             // get imu reading then convert to radians
@@ -105,17 +87,14 @@ public:
         // update current positon
         /* #region update position */
 
-        adjusted_rotation = inputModulus(position_rotation_sensor, -180, 180);
 
         position_x = position_x + (cos(position_rotation_rad) * deltaPos);
         position_y = position_y + (sin(position_rotation_rad) * deltaPos);
 
         /* #endregion */
     }
-    double get_imu_reading()
-    {
+    double get_imu_reading() {
         double reading_adjusted = (180 - imu.get_heading()); 
-
         // TODO:  might need to do PID stuff here.
         // TODO:  double reading_adjusted_pid = pid.calculate(current_reading_imu_pid, reading_adjusted);
         // TODO:  current_reading_imu_pid = reading_adjusted_pid;
