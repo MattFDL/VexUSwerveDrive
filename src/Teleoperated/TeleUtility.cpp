fda1b46eb1ee5e55abe78ffd9b649aa932c361b8
MATTHEW_JANKE <jankemat000@fondystudents.org>
2025-11-17 22:12:48 -0600

    Utility Class (for later use)

---
+++ src/Teleoperated/TeleUtility.cpp
@@ -0,0 +1,11 @@
+#include "TeleUtility.h"
+#include <cmath>
+
+/*
+This class should be used for operated control.
+These functions should make controlling the robot easier for operators
+For example, squaring inputs or adding a deadband onto controls
+*/
+double deadband(double in) {
+    return 0; //To be implemented later. 
+};
