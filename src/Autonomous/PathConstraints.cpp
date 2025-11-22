272d182880706770320685c466f93a3de866823e
MATTHEW_JANKE <jankemat000@fondystudents.org>
2025-11-22 13:39:42 -0600

    Look ahead algorithm (for pure pursuit path following)

--- src/Autonomous/PathConstraints.cpp
+++
@@ -1,9 +0,0 @@
-class PathContraints
-{
-public:
-    double accerleration;
-    double max_velocity;
-    double distance;
-    PathContraints(double accel, double max_vel, double dist) : accerleration(accel), max_velocity(max_vel), distance(dist) {}
-    
-};
--- src/Autonomous/PathFollower.cpp
+++ src/Autonomous/PathFollower.cpp
@@ -2,6 +2,8 @@
 #include "../odometry.cpp" //change these to headers eventually
 #include "pros/motor_group.hpp"
 #include "Path.cpp"
+#include "VelocityConstraints.cpp"
+#include "AutoUtility.h"
 
 class PathFollower
 {
@@ -11,7 +13,6 @@ private:
     Path path = Path();
     odometry odom;
 
-
     void driveMotor(double direction, double turn)
     {
         left_mg.move(direction - turn);
@@ -19,20 +20,58 @@ private:
     }
 
 public:
+    VelocityContraints constraints = VelocityContraints(5.0, 5.0);
+    int currentIndex = 0;
     
-    //std::vector<> curvePoints;
-
-    PathFollower(odometry& odometry, pros::MotorGroup &m_right, pros::MotorGroup &m_left) : odom(odometry), right_mg(m_right), left_mg(m_left)
-    {
+    PathFollower(odometry& odometr, pros::MotorGroup &m_right, pros::MotorGroup &m_left) : odom(odometr), right_mg(m_right), left_mg(m_left)
+    { 
 
     }
 
+    
     void setPath(Path m_path) {
         path = m_path;
     }
 
-    void setContraints() {
-        //TODO: create contraints class (acceleration, max_velocity)
+    void setContraints(double acceleration, double max_vel) {
+        constraints = VelocityContraints(acceleration, max_vel);
+    }
+
+    void followPath() {
+
+    }
+
+    int findLookAheadPoint(double lookahead, double x, double  y) {
+        //Get Current Index / Closest Point to the robots position
+        double closest_distance = std::numeric_limits<double>::infinity();
+        for (int i = currentIndex; i < (path.curvePoints.size() - 1); i++) {
+            double distance_current_to_next = calculateDistance(Point2D(x, y), path.curvePoints[i]);
+            if (distance_current_to_next < closest_distance) {
+                closest_distance = distance_current_to_next;
+                currentIndex = i;
+            }
+        }
+
+        currentIndex = std::max(0, currentIndex-1); //get previous index to account for delay in time not sure if needed
+        //----------------------------------------------//
+        //calculate the lookahead point 
+        //this is the point that the robot will attempt to move towards
+        int lookahead_index = currentIndex + 1;
+        double look_ahead_error = std::numeric_limits<double>::infinity();
+        for (int e = currentIndex; e < (path.curvePoints.size() - 1); e++) {
+            double distance_to_point = calculateDistance(Point2D(x, y), path.curvePoints[e]);//calculateDistance(path.curvePoints[currentIndex], path.curvePoints[e]);
+            double error = std::abs(distance_to_point-lookahead);
+            if (error < look_ahead_error) {
+                look_ahead_error = error;
+                lookahead_index = e;
+            }
+        } 
+        //This algorithm could technically be improved
+        //Think about it we could technically be stop searching for index's in the loop if we notice that the number's
+        //error keeps increasing 
+        //however, this algorithm 100% works
+        return lookahead_index;
+
     }
 
 };
